0d1bc76f47a22282071afd2b91bf50e3404bc958
antranig8 <antranig.t@gmail.com>
2025-07-28 22:15:21 -0400

    Add files via upload

---
+++ CMSC203_Lab6/CheckingAccount.java
@@ -0,0 +1,35 @@
+
+public class CheckingAccount extends BankAccount{
+	// fee for clearing one check
+	static final double FEE = 0.15; 
+	/*
+	 * This constructor initializes a CheckingAccount object with a given owner's name and initial balance.
+	 * It also appends "-10" to the account number to differentiate it from other account types.
+	 * @param name the name of the account owner
+	 * @param amount the initial balance of the account
+	 */
+	public CheckingAccount(String name, double amount) {
+		// Call superclass constructor
+		super(name, amount);
+		// Initialize the new account number
+		String updatedAccountNumber = getAccountNumber();
+		setAccountNumber(updatedAccountNumber + "-10");
+	}
+	
+	/*
+	 * This method allows the user to withdraw money from the checking account.
+	 * It deducts a fee for each check cleared.
+	 * @param amount the amount to withdraw
+	 * @return true if the withdrawal was successful, false if there are insufficient funds
+	 */
+	public boolean withdraw(double amount) {
+		boolean completed = false;
+		// Add the fee to the amount to be withdrawn
+		amount += FEE;
+		// Call the superclass withdraw method to attempt the withdrawal and set completed status
+		completed = super.withdraw(amount);
+		// Check if the withdrawal was successful
+		return completed;
+	}
+	
+}
---
+++ CMSC203_Lab6/SavingsAccount.java
@@ -0,0 +1,54 @@
+
+public class SavingsAccount extends BankAccount {
+	// Instance variable for interest rate
+	private double rate = 0.025;
+	// Instance variable for account number
+	private int savingsNumber = 0;
+	// Instance variable to hide the account number
+	private String accountNumber;
+	
+	/*
+	 * This constructor initilaizes a SavingsAccount object with a given owner's name and initial balance.
+	 * Calls for superclass constructor and also sets the account number
+	 * @param name the name of the account owner
+	 * @param amount the initial balance of the account
+	 */
+	public SavingsAccount(String name, double amount) {
+		// Call superclass constructor
+		super(name, amount);
+		// Set this account number to the superclass account number
+		accountNumber = super.getAccountNumber();
+		// Apped the account number with the savings number an hyphen
+		accountNumber += "-" + savingsNumber;
+	}
+	
+	/*
+	 * This method calculates one months worth of interest on the balance and deposit it into the account
+	 */
+	public void postInterest() {
+		double interest = getBalance() * rate / 12;
+		deposit(interest);
+	}
+	/*
+	 * Method that overrides the getAccountNumber method from the superclass
+	 * @return the account number of the savings account
+	 */
+	@Override
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	
+	/*
+	 * Copy constructor creates another account for the same owner
+	 * @param SavingsAccount oldAccount the account with information to copy
+	 * @param double amount the beginning balance of the new account
+	 */
+	public SavingsAccount(SavingsAccount oldAccount, double amount) {
+		// Call the superclass constructor
+		super(oldAccount, amount);
+		// // Set the account number for the new account
+		savingsNumber = oldAccount.savingsNumber + 1;
+		accountNumber = super.getAccountNumber() + "-" + savingsNumber;
+	}
+	
+}
